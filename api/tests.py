9cc45ed8b6d78dff9e425c581a5cacf50ddc2aca
mendesbayout <62385787+mendesbayout@users.noreply.github.com>
2021-03-23 19:42:04 -0300

    Update tests.py

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Cheff
 
-# Create your tests here.
+class Chefftest(TestCase):
+    def setUp(self):
+        Cheff.objects.create(id="Sanji", direction="Wano", phone='330', curriculum="a")
+        #Cheff.objects.create(id="Zeff", direction="East Blue", phone='001')
+                
+    def test_cheff(self):
+        Sanji =  Cheff.objects.get(id='Sanji')
+        Zeff = Cheff.objects.get(id="Zeff")
+        self.assertEqual(Sanji.speak(), "Nakamaaaa")
+        #self.assertEqual(Zeff.speak(), "sanjiiiiiii")
